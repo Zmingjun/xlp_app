896fefd12791c75780e277d34725242a440fc0c9
mingjun <13288912697@163.com>
2018-05-11 23:38:35 +0800

    五月份的提交

--- xlp_app/api.php
+++ xlp_app/api.php
@@ -14,7 +14,8 @@ header("content-type:text/html;charset=utf-8");
     header('Access-Control-Allow-Methods: POST,GET');  
     // 响应头设置  
     header('Access-Control-Allow-Headers:x-requested-with,content-type');  
-  $type=isset($_REQUEST['type'])?$_REQUEST['type']:1;exit;
+  $type=isset($_REQUEST['type'])?$_REQUEST['type']:1;
+  echo 123;
    
   
   switch ($type){
